67d75bc737cf5565837d8bfa5a62702e5818e7ac
Sander Verweij <sverweij@users.noreply.github.com>
2022-10-13 16:20:40 +0200

    refactor: use and emit esm (#9)

--- dist/cli.js
+++ dist/cli.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const commander_1 = require("commander");
-const version_js_1 = require("./version.js");
-const read_and_convert_js_1 = require("./read-and-convert.js");
-commander_1.program
+import { program } from "commander";
+import { VERSION } from "./version.js";
+import { readAndCovert } from "./read-and-convert.js";
+program
     .description("Takes a VIRTUAL-CODEOWNERS & a virtual-teams.yml and emits a CODEOWNERS to stdout")
-    .version(version_js_1.VERSION)
+    .version(VERSION)
     .arguments("<virtual-code-owners-file> <virtual-teams.yml>")
     .parse(process.argv);
 try {
-    console.log((0, read_and_convert_js_1.readAndCovert)(commander_1.program.args[0], commander_1.program.args[1]));
+    console.log(readAndCovert(program.args[0], program.args[1]));
 }
 catch (pError) {
     console.error(`ERROR: ${pError.message}`);
