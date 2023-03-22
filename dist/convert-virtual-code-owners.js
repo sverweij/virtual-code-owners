6b9a75f580437498d14d2285d3923a6013f5ac25
Sander Verweij <sverweij@users.noreply.github.com>
2023-03-22 21:37:48 +0100

    refactor: deduplicates line  parsing (#58)

--- dist/convert-virtual-code-owners.js
+++ dist/convert-virtual-code-owners.js
@@ -1,5 +1,5 @@
 import { EOL } from "node:os";
-const DEFAULT_GENERATED_WARNING = `#${EOL}` +
+const DEFAULT_WARNING = `#${EOL}` +
     `# DO NOT EDIT - this file is generated and your edits will be overwritten${EOL}` +
     `#${EOL}` +
     `# To make changes:${EOL}` +
@@ -8,14 +8,13 @@ const DEFAULT_GENERATED_WARNING = `#${EOL}` +
     `#   - and/ or add team members to .github/virtual-teams.yml${EOL}` +
     `#   - run 'npx virtual-code-owners'${EOL}` +
     `#${EOL}${EOL}`;
-const LINE_PATTERN = /^(?<filesPattern>[^\s]+\s+)(?<userNames>.*)$/;
-export function convert(pCodeOwnersFileAsString, pTeamMap, pGeneratedWarning = DEFAULT_GENERATED_WARNING) {
-    return `${pGeneratedWarning}${pCodeOwnersFileAsString
-        .split(EOL)
-        .filter(shouldAppearInResult)
-        .map(convertLine(pTeamMap))
-        .map(deduplicateUserNames)
-        .join(EOL)}`;
+export function convert(pCodeOwnersFileAsString, pTeamMap, pGeneratedWarning = DEFAULT_WARNING) {
+    return (pGeneratedWarning +
+        pCodeOwnersFileAsString
+            .split(EOL)
+            .filter(shouldAppearInResult)
+            .map(convertLine(pTeamMap))
+            .join(EOL));
 }
 function shouldAppearInResult(pLine) {
     return !pLine.trimStart().startsWith("#!");
@@ -23,34 +22,28 @@ function shouldAppearInResult(pLine) {
 function convertLine(pTeamMap) {
     return (pUntreatedLine) => {
         const lTrimmedLine = pUntreatedLine.trim();
-        if (lTrimmedLine.startsWith("#") || lTrimmedLine === "") {
+        const lSplitLine = lTrimmedLine.match(/^(?<filesPattern>[^\s]+\s+)(?<userNames>.*)$/);
+        if (lTrimmedLine.startsWith("#") ||
+            lTrimmedLine === "" ||
+            !lSplitLine?.groups) {
             return pUntreatedLine;
         }
-        else {
-            return replaceTeamNames(lTrimmedLine, pTeamMap);
-        }
+        const lUserNames = replaceTeamNames(lSplitLine.groups.userNames, pTeamMap);
+        return lSplitLine.groups.filesPattern + uniqAndSortUserNames(lUserNames);
     };
 }
-function replaceTeamNames(pTrimmedLine, pTeamMap) {
-    const lSplitLine = pTrimmedLine.match(LINE_PATTERN);
-    if (!lSplitLine?.groups) {
-        return pTrimmedLine;
-    }
-    let lUserNames = lSplitLine.groups.userNames.trim();
+function replaceTeamNames(pUserNames, pTeamMap) {
+    let lReturnValue = pUserNames;
     for (let lTeamName of Object.keys(pTeamMap)) {
-        lUserNames = lUserNames.replace(new RegExp(`(\\s|^)@${lTeamName}(\\s|$)`, "g"), `$1${stringifyTeamMembers(pTeamMap, lTeamName)}$2`);
+        lReturnValue = lReturnValue.replace(new RegExp(`(\\s|^)@${lTeamName}(\\s|$)`, "g"), `$1${stringifyTeamMembers(pTeamMap, lTeamName)}$2`);
     }
-    return `${lSplitLine.groups.filesPattern}${lUserNames}`;
+    return lReturnValue;
 }
 function stringifyTeamMembers(pTeamMap, pTeamName) {
     return pTeamMap[pTeamName].map((pUserName) => `@${pUserName}`).join(" ");
 }
-function deduplicateUserNames(pTrimmedLine) {
-    const lSplitLine = pTrimmedLine.match(LINE_PATTERN);
-    if (pTrimmedLine.startsWith("#") || !lSplitLine?.groups) {
-        return pTrimmedLine;
-    }
-    return `${lSplitLine.groups.filesPattern}${Array.from(new Set(lSplitLine.groups.userNames.trim().split(/\s+/)))
+function uniqAndSortUserNames(pUserNames) {
+    return Array.from(new Set(pUserNames.split(/\s+/)))
         .sort()
-        .join(" ")}`;
+        .join(" ");
 }
