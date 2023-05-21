b12fa82ecc0eacc1e470993ee1b63bf2b7e6de1d
Sander Verweij <sverweij@users.noreply.github.com>
2023-05-21 19:25:18 +0200

    refactor: replaces commander with native parseArgs BREAKING (#76)

--- dist/cli.js
+++ dist/cli.js
@@ -1,22 +1,69 @@
 #!/usr/bin/env node
-import { program } from "commander";
 import { VERSION } from "./version.js";
 import { readAndConvert } from "./read-and-convert.js";
 import { writeFileSync } from "node:fs";
 import { EOL } from "node:os";
-program
-    .description("Merges a VIRTUAL-CODEOWNERS.txt and a virtual-teams.yml into CODEOWNERS")
-    .version(VERSION)
-    .option("-v, --virtual-code-owners [file-name]", "A CODEOWNERS file with team names in them that are defined in a virtual teams file", ".github/VIRTUAL-CODEOWNERS.txt")
-    .option("-t, --virtual-teams [file-name]", "A YAML file listing teams and their members", ".github/virtual-teams.yml")
-    .option("-c, --code-owners [file-name]", "The location of the CODEOWNERS file", ".github/CODEOWNERS")
-    .parse(process.argv);
+import { parseArgs } from "node:util";
+let lOptions;
 try {
-    const lCodeOwnersContent = readAndConvert(program.opts().virtualCodeOwners, program.opts().virtualTeams);
-    writeFileSync(program.opts().codeOwners, lCodeOwnersContent, {
+    let lParsedArgs = parseArgs({
+        args: process.argv.slice(2),
+        options: {
+            virtualCodeOwners: {
+                type: "string",
+                short: "v",
+                default: ".github/VIRTUAL-CODEOWNERS.txt",
+            },
+            virtualTeams: {
+                type: "string",
+                short: "t",
+                default: ".github/virtual-teams.yml",
+            },
+            codeOwners: { type: "string", short: "c", default: ".github/CODEOWNERS" },
+            help: { type: "boolean", short: "h", default: false },
+            version: { type: "boolean", short: "V", default: false },
+        },
+        strict: true,
+        allowPositionals: true,
+        tokens: false,
+    });
+    lOptions = lParsedArgs.values;
+    if (lOptions.help) {
+        showHelp();
+        process.exit(0);
+    }
+    if (lOptions.version) {
+        console.log(VERSION);
+        process.exit(0);
+    }
+}
+catch (pError) {
+    console.error(`${EOL}ERROR: ${pError.message}${EOL}`);
+    process.exit(1);
+}
+function showHelp() {
+    console.log(`Usage: virtual-code-owners [options]
+
+Merges a VIRTUAL-CODEOWNERS.txt and a virtual-teams.yml into CODEOWNERS
+
+Options:
+  -V, --version                        output the version number
+  -v, --virtualCodeOwners [file-name]  A CODEOWNERS file with team names in them 
+                                       that are defined in a virtual teams file
+                                       (default: ".github/VIRTUAL-CODEOWNERS.txt")
+  -t, --virtualTeams [file-name]       A YAML file listing teams and their 
+                                       members
+                                       (default: ".github/virtual-teams.yml")
+  -c, --codeOwners [file-name]         The location of the CODEOWNERS file 
+                                       (default: ".github/CODEOWNERS")
+  -h, --help                           display help for command`);
+}
+try {
+    const lCodeOwnersContent = readAndConvert(lOptions.virtualCodeOwners, lOptions.virtualTeams);
+    writeFileSync(lOptions.codeOwners, lCodeOwnersContent, {
         encoding: "utf-8",
     });
-    console.error(`${EOL}Wrote ${program.opts().codeOwners}${EOL}`);
+    console.error(`${EOL}Wrote ${lOptions.codeOwners}${EOL}`);
 }
 catch (pError) {
     console.error(`ERROR: ${pError.message}`);
