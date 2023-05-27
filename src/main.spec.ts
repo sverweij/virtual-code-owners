9e889c53f6fec4d28b60d8687745b4482e797921
sverweij <sverweij@users.noreply.github.com>
2023-05-27 16:08:08 +0200

    refactor: makes cli testable

---
+++ src/main.spec.ts
@@ -0,0 +1,78 @@
+import { match } from "node:assert";
+import { main } from "./main.js";
+
+class WritableTestStream extends WritableStream {
+  expected = /^$/;
+
+  constructor(pExpected?: RegExp) {
+    super();
+    if (pExpected) {
+      this.expected = pExpected;
+    }
+  }
+  write(pChunk) {
+    match(pChunk, this.expected);
+    return true;
+  }
+}
+
+describe("main", () => {
+  it("shows the version number when asked for", () => {
+    let lOutStream = new WritableTestStream(/^[0-9]+\.[0-9]+\.[0-9]+(-.+)?\n$/);
+    let lErrStream = new WritableTestStream();
+    // @ts-expect-error can't assign WritableTestSTream to WriteStream
+    main(["-V"], lOutStream, lErrStream);
+    // @ts-expect-error can't assign WritableTestSTream to WriteStream
+    main(["--version"], lOutStream, lErrStream);
+  });
+
+  it("shows help when asked for", () => {
+    let lOutStream = new WritableTestStream(
+      /^Usage: virtual-code-owners \[options\].*/
+    );
+    let lErrStream = new WritableTestStream();
+    // @ts-expect-error can't assign WritableTestSTream to WriteStream
+    main(["-h"], lOutStream, lErrStream);
+    // @ts-expect-error can't assign WritableTestSTream to WriteStream
+    main(["--help"], lOutStream, lErrStream);
+  });
+
+  it("shows an error when passed a non-existing argument", () => {
+    let lOutStream = new WritableTestStream();
+    let lErrStream = new WritableTestStream(
+      /.*ERROR:.*'--thisArgumentDoesNotExist'.*/
+    );
+    // @ts-expect-error can't assign WritableTestSTream to WriteStream
+    main(["--thisArgumentDoesNotExist"], lOutStream, lErrStream);
+  });
+
+  it("shows an error when passed a non-existing file", () => {
+    let lOutStream = new WritableTestStream();
+    let lErrStream = new WritableTestStream(/.*ERROR: ENOENT:.*/);
+    // @ts-expect-error can't assign WritableTestSTream to WriteStream
+    main(["-v", "this-file-does-not-exist.txt"], lOutStream, lErrStream);
+  });
+
+  it("ignores positional arguments", () => {
+    let lOutStream = new WritableTestStream();
+    let lErrStream = new WritableTestStream(
+      /.*Wrote node_modules\/tmp-code-owners\.txt.*/
+    );
+    main(
+      [
+        "--virtualCodeOwners",
+        "./src/__mocks__/VIRTUAL-CODEOWNERS.txt",
+        "--virtualTeams",
+        "./src/__mocks__/virtual-teams.yml",
+        "--codeOwners",
+        "node_modules/tmp-code-owners.txt",
+        "few",
+        "positional",
+        "arguments",
+      ],
+      // @ts-expect-error can't assign WritableTestSTream to WriteStream
+      lOutStream,
+      lErrStream
+    );
+  });
+});
---
+++ src/main.ts
@@ -0,0 +1,89 @@
+import { VERSION } from "./version.js";
+import { readAndConvert } from "./read-and-convert.js";
+import { writeFileSync } from "node:fs";
+import { EOL } from "node:os";
+import { parseArgs } from "node:util";
+
+interface IOptions {
+  virtualCodeOwners: string;
+  virtualTeams: string;
+  codeOwners: string;
+  help: boolean;
+  version: boolean;
+}
+const HELP_MESSAGE = `Usage: virtual-code-owners [options]
+
+Merges a VIRTUAL-CODEOWNERS.txt and a virtual-teams.yml into CODEOWNERS
+
+Options:
+  -V, --version                        output the version number
+  -v, --virtualCodeOwners [file-name]  A CODEOWNERS file with team names in them 
+                                       that are defined in a virtual teams file
+                                       (default: ".github/VIRTUAL-CODEOWNERS.txt")
+  -t, --virtualTeams [file-name]       A YAML file listing teams and their 
+                                       members
+                                       (default: ".github/virtual-teams.yml")
+  -c, --codeOwners [file-name]         The location of the CODEOWNERS file 
+                                       (default: ".github/CODEOWNERS")
+  -h, --help                           display help for command`;
+
+export function main(
+  pArguments: string[] = process.argv.slice(2),
+  pOutStream: NodeJS.WriteStream = process.stdout,
+  pErrorStream: NodeJS.WriteStream = process.stderr
+) {
+  try {
+    let lOptions = getOptions(pArguments);
+
+    if (lOptions.help) {
+      pOutStream.write(`${HELP_MESSAGE}${EOL}`);
+      return;
+    }
+    if (lOptions.version) {
+      pOutStream.write(`${VERSION}${EOL}`);
+      return;
+    }
+
+    const lCodeOwnersContent = readAndConvert(
+      lOptions.virtualCodeOwners,
+      lOptions.virtualTeams
+    );
+    writeFileSync(lOptions.codeOwners, lCodeOwnersContent, {
+      encoding: "utf-8",
+    });
+    pErrorStream.write(`${EOL}Wrote ${lOptions.codeOwners}${EOL}${EOL}`);
+  } catch (pError: any) {
+    pErrorStream.write(`${EOL}ERROR: ${pError.message}${EOL}${EOL}`);
+    process.exitCode = 1;
+  }
+}
+
+function getOptions(pArguments: string[]): IOptions {
+  return parseArgs({
+    args: pArguments,
+    options: {
+      virtualCodeOwners: {
+        type: "string",
+        short: "v",
+        default: ".github/VIRTUAL-CODEOWNERS.txt",
+      },
+      virtualTeams: {
+        type: "string",
+        short: "t",
+        default: ".github/virtual-teams.yml",
+      },
+      codeOwners: {
+        type: "string",
+        short: "c",
+        default: ".github/CODEOWNERS",
+      },
+      help: { type: "boolean", short: "h", default: false },
+      version: { type: "boolean", short: "V", default: false },
+    },
+    strict: true,
+    // positionals allowed so lint staged can pass them on the cli.
+    // We will, however, ignore them.
+    allowPositionals: true,
+    tokens: false,
+  }).values as IOptions;
+}
