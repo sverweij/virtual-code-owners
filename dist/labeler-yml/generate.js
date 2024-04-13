7d758f1c9c84a8f5a5f40d435d03a3bc4ef85f85
sverweij <sverweij@users.noreply.github.com>
2024-04-13 11:39:22 +0200

    chore: tabs < spaces
    it makes the (unpacked) distribution a few hundred bytes smaller

--- dist/labeler-yml/generate.js
+++ dist/labeler-yml/generate.js
@@ -1,56 +1,56 @@
 import { EOL } from "node:os";
 const DEFAULT_WARNING =
-  `#${EOL}` +
-  `# DO NOT EDIT - this file is generated and your edits will be overwritten${EOL}` +
-  `#${EOL}` +
-  `# To make changes:${EOL}` +
-  `#${EOL}` +
-  `#   - edit .github/VIRTUAL-CODEOWNERS.txt${EOL}` +
-  `#   - and/ or add teams (& members) to .github/virtual-teams.yml${EOL}` +
-  `#   - run 'npx virtual-code-owners --emitLabeler'${EOL}` +
-  `#${EOL}${EOL}`;
+	`#${EOL}` +
+	`# DO NOT EDIT - this file is generated and your edits will be overwritten${EOL}` +
+	`#${EOL}` +
+	`# To make changes:${EOL}` +
+	`#${EOL}` +
+	`#   - edit .github/VIRTUAL-CODEOWNERS.txt${EOL}` +
+	`#   - and/ or add teams (& members) to .github/virtual-teams.yml${EOL}` +
+	`#   - run 'npx virtual-code-owners --emitLabeler'${EOL}` +
+	`#${EOL}${EOL}`;
 export default function generateLabelerYml(
-  pCodeOwners,
-  pTeamMap,
-  pGeneratedWarning = DEFAULT_WARNING,
+	pCodeOwners,
+	pTeamMap,
+	pGeneratedWarning = DEFAULT_WARNING,
 ) {
-  let lReturnValue = pGeneratedWarning;
-  for (const lTeamName in pTeamMap) {
-    const lPatternsForTeam = getPatternsForTeam(pCodeOwners, lTeamName)
-      .map(
-        (pPattern) =>
-          `      - any-glob-to-any-file: ${transformForYamlAndMinimatch(pPattern)}${EOL}`,
-      )
-      .join("");
-    if (lPatternsForTeam) {
-      lReturnValue += `${lTeamName}:${EOL}  - changed-files:${EOL}${lPatternsForTeam}${EOL}`;
-    }
-  }
-  return lReturnValue;
+	let lReturnValue = pGeneratedWarning;
+	for (const lTeamName in pTeamMap) {
+		const lPatternsForTeam = getPatternsForTeam(pCodeOwners, lTeamName)
+			.map(
+				(pPattern) =>
+					`      - any-glob-to-any-file: ${transformForYamlAndMinimatch(pPattern)}${EOL}`,
+			)
+			.join("");
+		if (lPatternsForTeam) {
+			lReturnValue += `${lTeamName}:${EOL}  - changed-files:${EOL}${lPatternsForTeam}${EOL}`;
+		}
+	}
+	return lReturnValue;
 }
 function getPatternsForTeam(pCodeOwners, pTeamName) {
-  return pCodeOwners
-    .filter((pLine) => {
-      return pLine.type === "rule" && lineContainsTeamName(pLine, pTeamName);
-    })
-    .map((pLine) => pLine.filesPattern);
+	return pCodeOwners
+		.filter((pLine) => {
+			return pLine.type === "rule" && lineContainsTeamName(pLine, pTeamName);
+		})
+		.map((pLine) => pLine.filesPattern);
 }
 function transformForYamlAndMinimatch(pOriginalString) {
-  let lReturnValue = pOriginalString;
-  if (pOriginalString === "*") {
-    lReturnValue = "**";
-  }
-  if (lReturnValue.startsWith("*")) {
-    lReturnValue = `"${lReturnValue}"`;
-  }
-  if (pOriginalString.endsWith("/")) {
-    lReturnValue = `${lReturnValue}**`;
-  }
-  return lReturnValue;
+	let lReturnValue = pOriginalString;
+	if (pOriginalString === "*") {
+		lReturnValue = "**";
+	}
+	if (lReturnValue.startsWith("*")) {
+		lReturnValue = `"${lReturnValue}"`;
+	}
+	if (pOriginalString.endsWith("/")) {
+		lReturnValue = `${lReturnValue}**`;
+	}
+	return lReturnValue;
 }
 function lineContainsTeamName(pLine, pTeamName) {
-  return pLine.users.some(
-    (pUser) =>
-      pUser.type === "virtual-team-name" && pUser.bareName === pTeamName,
-  );
+	return pLine.users.some(
+		(pUser) =>
+			pUser.type === "virtual-team-name" && pUser.bareName === pTeamName,
+	);
 }
--- dist/main.js
+++ dist/main.js
@@ -5,30 +5,30 @@ import generateLabelerYml from "./labeler-yml/generate.js";
 import readTeamMap from "./team-map/read.js";
 import readVirtualCodeOwners from "./virtual-code-owners/read.js";
 export function main(pOptions, pErrorStream) {
-  const lTeamMap = readTeamMap(pOptions.virtualTeams);
-  const lVirtualCodeOwners = readVirtualCodeOwners(
-    pOptions.virtualCodeOwners,
-    lTeamMap,
-  );
-  const lCodeOwnersContent = generateCodeOwners(lVirtualCodeOwners, lTeamMap);
-  if (!pOptions.dryRun) {
-    writeFileSync(pOptions.codeOwners, lCodeOwnersContent, {
-      encoding: "utf-8",
-      flag: "w",
-    });
-  }
-  if (pOptions.emitLabeler) {
-    const lLabelerContent = generateLabelerYml(lVirtualCodeOwners, lTeamMap);
-    if (!pOptions.dryRun) {
-      writeFileSync(pOptions.labelerLocation, lLabelerContent, {
-        encoding: "utf-8",
-        flag: "w",
-      });
-    }
-    pErrorStream.write(
-      `${EOL}Wrote '${pOptions.codeOwners}' AND '${pOptions.labelerLocation}'${EOL}${EOL}`,
-    );
-  } else {
-    pErrorStream.write(`${EOL}Wrote '${pOptions.codeOwners}'${EOL}${EOL}`);
-  }
+	const lTeamMap = readTeamMap(pOptions.virtualTeams);
+	const lVirtualCodeOwners = readVirtualCodeOwners(
+		pOptions.virtualCodeOwners,
+		lTeamMap,
+	);
+	const lCodeOwnersContent = generateCodeOwners(lVirtualCodeOwners, lTeamMap);
+	if (!pOptions.dryRun) {
+		writeFileSync(pOptions.codeOwners, lCodeOwnersContent, {
+			encoding: "utf-8",
+			flag: "w",
+		});
+	}
+	if (pOptions.emitLabeler) {
+		const lLabelerContent = generateLabelerYml(lVirtualCodeOwners, lTeamMap);
+		if (!pOptions.dryRun) {
+			writeFileSync(pOptions.labelerLocation, lLabelerContent, {
+				encoding: "utf-8",
+				flag: "w",
+			});
+		}
+		pErrorStream.write(
+			`${EOL}Wrote '${pOptions.codeOwners}' AND '${pOptions.labelerLocation}'${EOL}${EOL}`,
+		);
+	} else {
+		pErrorStream.write(`${EOL}Wrote '${pOptions.codeOwners}'${EOL}${EOL}`);
+	}
 }
