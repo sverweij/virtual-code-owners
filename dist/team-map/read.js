cb498e868ae753006ab3791258bff600fa4e9393
sverweij <sverweij@users.noreply.github.com>
2024-04-26 21:07:55 +0200

    refactor(team-map): replaces 'ajv' with a simple hand validation

--- dist/team-map/read.js
+++ dist/team-map/read.js
@@ -1,8 +1,6 @@
-import Ajv from "ajv";
 import { readFileSync } from "node:fs";
-import { EOL } from "node:os";
 import { parse as parseYaml } from "yaml";
-import virtualTeamsSchema from "./virtual-teams.schema.js";
+import { EOL } from "node:os";
 export default function readTeamMap(pVirtualTeamsFileName) {
 	const lVirtualTeamsAsAString = readFileSync(pVirtualTeamsFileName, {
 		encoding: "utf-8",
@@ -11,22 +9,74 @@ export default function readTeamMap(pVirtualTeamsFileName) {
 	assertTeamMapValid(lTeamMap, pVirtualTeamsFileName);
 	return lTeamMap;
 }
-function assertTeamMapValid(pTeamMap, pVirtualTeamsFileName) {
-	const ajv = new Ajv({
-		allErrors: true,
-		verbose: true,
-	});
-	if (!ajv.validate(virtualTeamsSchema, pTeamMap)) {
+function assertTeamMapValid(pTeamMapCandidate, pVirtualTeamsFileName) {
+	const [lValid, lError] = validateTeamMap(pTeamMapCandidate);
+	if (!lValid) {
 		throw new Error(
-			`This is not a valid virtual-teams.yml:${EOL}${formatAjvErrors(ajv.errors, pVirtualTeamsFileName)}.\n`,
+			`'${pVirtualTeamsFileName}' is not a valid virtual-teams.yml:${EOL}  ${lError}`,
 		);
 	}
 }
-function formatAjvErrors(pAjvErrors, pVirtualTeamsFileName) {
-	return pAjvErrors
-		.map((pAjvError) => formatAjvError(pAjvError, pVirtualTeamsFileName))
-		.join(EOL);
+function validateTeamMap(pCandidateTeamMap) {
+	if (
+		typeof pCandidateTeamMap !== "object" ||
+		pCandidateTeamMap === null ||
+		Array.isArray(pCandidateTeamMap)
+	) {
+		return [false, "The team map is not an object"];
+	}
+	const lTeamNameResults = Object.keys(pCandidateTeamMap).map(validateTeamName);
+	const lErrors = lTeamNameResults.filter((result) => !result[0]);
+	if (lErrors.length > 0) {
+		return [
+			false,
+			`These team names are not valid: ${lErrors.map((error) => error[1]).join(", ")}`,
+		];
+	}
+	const lTeamResults = Object.keys(pCandidateTeamMap).map((pKey) =>
+		validateTeam(pCandidateTeamMap[pKey], pKey),
+	);
+	const lTeamErrors = lTeamResults.filter((result) => !result[0]);
+	if (lTeamErrors.length > 0) {
+		return [false, lTeamErrors.map((error) => error[1]).join(`, ${EOL}  `)];
+	}
+	return [true];
 }
-function formatAjvError(pAjvError, pVirtualTeamsFileName) {
-	return `${pVirtualTeamsFileName}: ${pAjvError.instancePath} - ${JSON.stringify(pAjvError.data)} ${pAjvError.message}`;
+function validateTeamName(pTeamNameCandidate) {
+	if (typeof pTeamNameCandidate !== "string") {
+		return [false, "not a string"];
+	}
+	if (pTeamNameCandidate === "") {
+		return [false, "'' (empty string)"];
+	}
+	if (pTeamNameCandidate.includes(" ")) {
+		return [false, `'${pTeamNameCandidate}' (contains spaces)`];
+	}
+	return [true];
+}
+function validateTeam(pCandidateTeam, pTeamName) {
+	if (!Array.isArray(pCandidateTeam)) {
+		return [false, `This team is not an array: '${pTeamName}'`];
+	}
+	const lTeamMemberResults = pCandidateTeam.map(validateTeamMember);
+	const lErrors = lTeamMemberResults.filter((result) => !result[0]);
+	if (lErrors.length > 0) {
+		return [false, lErrors.map((error) => error[1]).join(", ")];
+	}
+	return [true];
+}
+function validateTeamMember(pTeamMemberCandidate) {
+	if (typeof pTeamMemberCandidate !== "string") {
+		return [
+			false,
+			`This username is not a string: '${pTeamMemberCandidate.toString()}'`,
+		];
+	}
+	if (!/^[^@][^\s]+$/.test(pTeamMemberCandidate)) {
+		return [
+			false,
+			`This username doesn't match /^[^@][^\\s]+$/: '${pTeamMemberCandidate}'`,
+		];
+	}
+	return [true];
 }
