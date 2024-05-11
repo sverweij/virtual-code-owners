9f61fb9289293b62431fbdab0747e6df9283b4ff
Sander Verweij <sverweij@users.noreply.github.com>
2024-05-11 12:33:34 +0200

    chore: replaces c8 with node native  coverage (#203)
    ## Description
    
    - removes c8 dependency &  config
    - uses  the --experimental-test-coverage cli switch to get coverage
    - updates `./tools/dot-with-summary.reporter.js` reporter to emit
    coverage information
    - updates the ci workflow to emit information from that report instead
    of the markdown derived from the codecov run (on node 20 only for now as
    there it's stable).
    
    ## Motivation and Context
    
    In our setup c8 doesn't provide any real coverage information anymore
    (see below - note the number of lines, functions and branches). Root
    cause probably lies [how node handles ESM/ how v8-to-istanbul can
    process that](https://github.com/bcoe/c8/issues/34). As nodejs now also
    has a --experimental-test-coverage built in we migrate this to that.
    
    > --experimental-test-coverage isn't perfect either b.t.w. In node 20
    it's stable, but on node 22 (at least in our set up) it tends to report
    different results on each run with unchanged source code.
    
    <details>
    <summary>recent c8 output on virtual-code-owners main branch</summary>
    
    ```shell
    $ npm test
    
    > virtual-code-owners@8.0.5 test
    > c8 tsx --test-reporter ./tools/dot-with-summary.reporter.js --test src/*.test.ts src/**/*.test.ts
    
    .....................................................
    
    53 passing (1.159 ms)
    
    =============================== Coverage summary ===============================
    Statements   : 100% ( 11/11 )
    Branches     : 100% ( 0/0 )
    Functions    : 100% ( 0/0 )
    Lines        : 100% ( 11/11 )
    ================================================================================
    ```
    
    </details>
    
    ## How Has This Been Tested?
    
    - [x] green ci
    - [x] manually (the script in tools should probably be put in a separate
    repo and get its own set of unit tests c.s., but for fixing the issue at
    hand a bit out of scope.
    
    ## Screenshots
    
    ```
    .....................................................
    
    53 passing (1.199 ms)
    
    =============================== Coverage summary ===============================
    Branches     : 100 % (205/205)
    Functions    : 100 % (123/123)
    Lines        : 100 % (1.533/1.533)
    ================================================================================
    ```
    
    With uncovered lines/ functions (b.t.w. same, unchanged codebase as
    above ..., but using node 22, after a run or two)
    
    ```shell
    .....................................................
    
    53 passing (1.216 ms)
    
    =============================== Coverage summary ===============================
    Branches     : 100 % (184/184)
    Functions    : 98,31 % (116/118) NOK
    Lines        : 99,74 % (1.529/1.533)
    ================================================================================
    
    Uncovered lines:
      /Users/sander/prg/js/virtual-code-owners/src/labeler-yml/generate.ts:102,103,104
      /Users/sander/prg/js/virtual-code-owners/src/virtual-code-owners/anomalies.ts:40
    
    Uncovered functions:
      /Users/sander/prg/js/virtual-code-owners/src/virtual-code-owners/anomalies.ts:40,40
    ```
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] Documentation only change
    - [ ] Refactor (non-breaking change which fixes an issue without
    changing functionality)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [x] Chore (thing that needs doing in e.g. coding infrastructure)

--- tools/dot-with-summary.reporter.js
+++ tools/dot-with-summary.reporter.js
@@ -9,11 +9,22 @@ const gTimeFormat = new Intl.NumberFormat(LOCALE, {
   unitDisplay: "narrow",
   maximumFractionDigits: 0,
 }).format;
+const gPercentFormat = new Intl.NumberFormat(LOCALE, {
+  style: "percent",
+  maximumFractionDigits: 2,
+  notation: "compact",
+}).format;
+const gNumberFormat = new Intl.NumberFormat(LOCALE).format;
+
+const BRANCH_COVERAGE_THRESHOLD = 99;
+const FUNCTION_COVERAGE_THRESHOLD = 99;
+const LINE_COVERAGE_THRESHOLD = 99;
 
 // eslint-disable-next-line max-lines-per-function, complexity
 export default async function* dotWithSummaryReporter(pSource) {
   let lFailStack = [];
   let lDiagnosticStack = [];
+  let lCoverageObject = {};
 
   for await (const lEvent of pSource) {
     switch (lEvent.type) {
@@ -46,7 +57,7 @@ export default async function* dotWithSummaryReporter(pSource) {
         break;
       // test:coverage apparently exists, but not seen in any runs so far
       case "test:coverage":
-        yield "C";
+        lCoverageObject = lEvent;
         break;
       default:
         break;
@@ -59,14 +70,120 @@ export default async function* dotWithSummaryReporter(pSource) {
 
   yield `${
     EOL + lFailStack.map(summarizeFailsToText).filter(Boolean).join(EOL)
-  }${EOL}${lDiagnostics.pass} passing (${gTimeFormat(
-    lDiagnostics.duration_ms,
-  )})${EOL}` +
-    `${lDiagnostics.fail > 0 ? `${lDiagnostics.fail} failing${EOL}` : ""}` +
-    `${
-      lDiagnostics.skipped > 0 ? `${lDiagnostics.skipped} skipped${EOL}` : ""
-    }` +
-    `${lDiagnostics.todo > 0 ? `${lDiagnostics.todo} todo${EOL}` : ""}${EOL}`;
+  }${EOL}` +
+    summarizeCounts(lDiagnostics) +
+    EOL +
+    summarizeCoverage(lCoverageObject) +
+    EOL +
+    getCoverageLines(lCoverageObject) +
+    getCoverageFunctions(lCoverageObject);
+
+  process.exitCode = determineExitCode(lFailStack, lCoverageObject);
+}
+
+function summarizeCoverage(pCoverageObject) {
+  let lCoverageSummary = "";
+
+  if (pCoverageObject?.data?.summary?.totals) {
+    const lTotals = pCoverageObject.data.summary.totals;
+    // console.log(lTotals);
+    lCoverageSummary =
+      "=============================== Coverage summary ===============================" +
+      EOL +
+      `Branches     : ${gPercentFormat(lTotals.coveredBranchCount / lTotals.totalBranchCount)} (${gNumberFormat(lTotals.coveredBranchCount)}/${gNumberFormat(lTotals.totalBranchCount)})` +
+      `${lTotals.coveredBranchPercent < BRANCH_COVERAGE_THRESHOLD ? " NOK" : ""}` +
+      EOL +
+      `Functions    : ${gPercentFormat(lTotals.coveredFunctionCount / lTotals.totalFunctionCount)} (${gNumberFormat(lTotals.coveredFunctionCount)}/${gNumberFormat(lTotals.totalFunctionCount)})` +
+      `${lTotals.coveredFunctionPercent < FUNCTION_COVERAGE_THRESHOLD ? " NOK" : ""}` +
+      EOL +
+      `Lines        : ${gPercentFormat(lTotals.coveredLineCount / lTotals.totalLineCount)} (${gNumberFormat(lTotals.coveredLineCount)}/${gNumberFormat(lTotals.totalLineCount)})` +
+      `${lTotals.coveredLinePercent < LINE_COVERAGE_THRESHOLD ? " NOK" : ""}` +
+      EOL +
+      "================================================================================" +
+      EOL;
+  }
+  return lCoverageSummary;
+}
+
+function getCoverageLines(pCoverageObject) {
+  const lUncoveredLinesPerFiles = (pCoverageObject?.data?.summary?.files ?? [])
+    .filter((pSummary) => pSummary.coveredLinePercent < 100)
+    .map(
+      (pSummary) =>
+        "  " +
+        pSummary.path +
+        ":" +
+        (pSummary.lines || [])
+          .filter((pLine) => pLine.count <= 0)
+          .map((pLine) => pLine.line)
+          .join(","),
+    );
+  if (lUncoveredLinesPerFiles.length === 0) {
+    return "";
+  }
+  return "Uncovered lines:" + EOL + lUncoveredLinesPerFiles.join(EOL) + EOL;
+}
+
+function getCoverageFunctions(pCoverageObject) {
+  const lUncoveredFunctionsPerFiles = (
+    pCoverageObject?.data?.summary?.files ?? []
+  )
+    .filter((pSummary) => pSummary.coveredFunctionPercent < 100)
+    .map(
+      (pSummary) =>
+        "  " +
+        pSummary.path +
+        ":" +
+        (pSummary.functions || [])
+          .filter((pFunction) => pFunction.count <= 0)
+          .map(
+            (pFunction) =>
+              `${pFunction.line}${pFunction.name ? `(${pFunction.name})` : ""}`,
+          )
+          .join(","),
+    );
+  if (lUncoveredFunctionsPerFiles.length === 0) {
+    return "";
+  }
+  return (
+    EOL +
+    "Uncovered functions:" +
+    EOL +
+    lUncoveredFunctionsPerFiles.join(EOL) +
+    EOL
+  );
+}
+
+function summarizeCounts(pDiagnostics) {
+  return (
+    `${gNumberFormat(pDiagnostics.pass)} passing (${gTimeFormat(pDiagnostics.duration_ms)})` +
+    EOL +
+    `${pDiagnostics.fail > 0 ? `${pDiagnostics.fail} failing${EOL}` : ""}` +
+    `${pDiagnostics.skipped > 0 ? `${pDiagnostics.skipped} skipped${EOL}` : ""}` +
+    `${pDiagnostics.todo > 0 ? `${pDiagnostics.todo} todo${EOL}` : ""}`
+  );
+}
+
+function determineExitCode(pFailStack, pCoverageObject) {
+  if (pFailStack.length > 0) {
+    return 1;
+  }
+  if (
+    (pCoverageObject?.data?.summary?.totals.coveredBranchPercent ?? 100) <
+      BRANCH_COVERAGE_THRESHOLD ||
+    (pCoverageObject?.data?.summary?.totals.coveredFunctionPercent ?? 100) <
+      FUNCTION_COVERAGE_THRESHOLD ||
+    (pCoverageObject?.data?.summary?.totals.coveredLinePercent ?? 100) <
+      LINE_COVERAGE_THRESHOLD
+  ) {
+    // should return  1, but on node 22 with our setup at least coverage
+    // runs can vary per run so even when the tests _do_ cover 100%, the
+    // coverage might be reported as something lower.
+    //
+    // So far b.t.w. the results on node 20 _are_ consistent.
+    return 0;
+  }
+  return 0;
 }
 
 function summarizeFailsToText(pFailEvent) {
